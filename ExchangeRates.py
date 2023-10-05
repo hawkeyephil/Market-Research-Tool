caa56bce69451eefed2f7c103731198f2fefab05
hawkeyephil <hawkeyephil@icloud.com>
2023-10-05 17:27:05 -0400

    Column Revisions

--- ExchangeRates.py
+++ ExchangeRates.py
@@ -5,19 +5,19 @@ from IPython.display import display
 from tabulate import tabulate 
 
 #Create a new dataframe to store results
-exchange_Rate_Data = panda.DataFrame(columns=['Currencies', 'Currency Code', 'Date', 'Exchange Rate'])
+exchange_Rate_Data = panda.DataFrame(columns=['Currency', 'Currency Code', 'Date', 'Exchange Rate to USD'])
 
 #Currency codes for X Currency to U.S. Dollars spot exchange rate from the FRED database 
-currency_Codes_ToUSD = [('JapaneseYen-USD', 'DEXJPUS'), ('CanadianDollar-USD', 'DEXCAUS'), ('SwissFranc-USD', 'DEXSZUS')] 
+currency_Codes_ToUSD = [('Japanese Yen', 'DEXJPUS'), ('Canadian Dollar', 'DEXCAUS'), ('Swiss Franc', 'DEXSZUS')] 
 #Currency codes for U.S. Dollars to X Currecny spot exchange rate from the FRED database 
-currency_Codes_FromUSD = [('Euro-USD', 'DEXUSEU'), ('Pounds-USD', 'DEXUSUK'), ('AustralianDollars-USD', 'DEXUSAL')] 
+currency_Codes_FromUSD = [('Euro', 'DEXUSEU'), ('Pound Sterling', 'DEXUSUK'), ('Australian Dollar', 'DEXUSAL')] 
 
 #Fetches the exchange rate data for specified currencies and adds the most recent observation to the data frame
 for name, currency_Code in currency_Codes_ToUSD: 
     exchange_Rate = reader.DataReader(currency_Code, 'fred') 
     exchange_Rate_Recent = exchange_Rate.tail(1)[currency_Code][0] 
     exchange_Rate_Date = exchange_Rate.tail(1).index.strftime('%Y-%m-%d')[0] 
-    new_Row = {'Currencies': name, 'Currency Code': currency_Code, 'Date': exchange_Rate_Date, 'Exchange Rate': exchange_Rate_Recent} 
+    new_Row = {'Currency': name, 'Currency Code': currency_Code, 'Date': exchange_Rate_Date, 'Exchange Rate to USD': exchange_Rate_Recent} 
     exchange_Rate_Data = exchange_Rate_Data.append(new_Row, ignore_index=True) 
 
 #Fetches the exchange rate data for specified currencies, inverts to USD, and adds the most recent observation to the data frame
@@ -26,7 +26,7 @@ for name, currency_Code in currency_Codes_FromUSD:
     exchange_Rate_Recent = exchange_Rate.tail(1)[currency_Code][0] 
     exchange_Rate_Recent = 1/exchange_Rate_Recent
     exchange_Rate_Date = exchange_Rate.tail(1).index.strftime('%Y-%m-%d')[0] 
-    new_Row = {'Currencies': name, 'Currency Code': currency_Code, 'Date': exchange_Rate_Date, 'Exchange Rate': exchange_Rate_Recent} 
+    new_Row = {'Currency': name, 'Currency Code': currency_Code, 'Date': exchange_Rate_Date, 'Exchange Rate to USD': exchange_Rate_Recent} 
     exchange_Rate_Data = exchange_Rate_Data.append(new_Row, ignore_index=True) 
 
 
