91954867812be7e28d23e34b344713b35ecfb1c6
hawkeyephil <hawkeyephil@icloud.com>
2024-01-14 01:28:35 -0500

    Working Report

--- Euro-Atlantic.py
+++ Euro-Atlantic.py
@@ -5,51 +5,37 @@ import numpy as numpy
 from IPython.display import display 
 from tabulate import tabulate 
 
-#Create a new dataframe to store results
-exchange_Rate_Data = panda.DataFrame(columns=['Economy', 'Currency', 'Exchange Rate to USD', 
-                'Exchange Rate Date', 'GDP', 'GDP Date'])#, 'Inflation Rate', 'Inflation Rate Date'])
+euro_Atlantic_Codes = [('Canada', 'Canadian Dollar', 'DEXCAUS', 'fred', 'NGDPRSAXDCCAQ', 'fred', 'CANCPALTT01CTGYM', 'fred'), ('Switzerland', 'Swiss Franc', 'DEXSZUS', 'fred', 'CPMNACSAB1GQCH', 'fred', 'CHECPALTT01CTGYM', 'fred')]
 
-#Currency codes for X Currency to U.S. Dollars spot exchange rate from the FRED database 
-#currency_Codes_ToUSD = [('Canadian Dollar', 'DEXCAUS'), ('Swiss Franc', 'DEXSZUS')] 
-economey_Codes = [('Canada', 'Canadian Dollar', 'DEXCAUS', 'NGDPRSAXDCCAQ', 'CANCPALTT01CTGYM'), ('Switzerland', 'Swiss Franc', 'DEXSZUS', 'CPMNACSAB1GQCH', 'CHECPALTT01CTGYM')]
-#Currency codes for U.S. Dollars to X Currecny spot exchange rate from the FRED database 
-#currency_Codes_FromUSD = [('Euro', 'DEXUSEU'), ('Pound Sterling', 'DEXUSUK')] 
-
-#Fetches the exchange rate data for specified currencies and adds the most recent observation to the data frame
-for economy, currency, currency_Code, output_Code, inflation_Code in economey_Codes: 
-    exchange_Rate = reader.DataReader(currency_Code, 'fred') 
-    output = reader.DataReader(output_Code, 'fred') 
-    exchange_Rate_Recent = exchange_Rate.tail(1)[currency_Code][0] 
-    output_Recent = output.tail(1)[output_Code][0]
-    exchange_Rate_Date = exchange_Rate.tail(1).index.strftime('%Y-%m-%d')[0] 
-    output_Date = output.tail(1).index.strftime('%Y-%m-%d')[0]
-    new_Row = {'Economy': economy, 'Currency': currency, 'Exchange Rate to USD': exchange_Rate_Recent, 'Exchange Rate Date': exchange_Rate_Date, 'GDP': output_Recent, 'GDP Date': output_Date} 
-    exchange_Rate_Data = exchange_Rate_Data.append(new_Row, ignore_index=True) 
- 
 def macro_Variable_Collector(macro_Variable_Codes): 
     macro_Variables_Dates = []
     for macro_Variable_Code, macro_Variable_Source in macro_Variable_Codes: 
         macro_Variable = reader.DataReader(macro_Variable_Code, macro_Variable_Source) 
         macro_Variable_Recent = macro_Variable.tail(1)[macro_Variable_Code][0] 
+        macro_Variable_Recent = "{:.4g}".format(macro_Variable_Recent)
         macro_Variable_Date = macro_Variable.tail(1).index.strftime('%Y-%m-%d')[0] 
-        macro_VariableDate = str(macro_Variable_Recent) + ' ' + str(macro_Variable_Date)
+        macro_VariableDate = str(macro_Variable_Recent) + ' (' + str(macro_Variable_Date) + ')'
         macro_Variables_Dates.append(macro_VariableDate) 
-    print(macro_Variables_Dates)
     return macro_Variables_Dates 
 
-test = [('DEXCAUS', 'fred'), ('DEXSZUS', 'fred')]
-macro_Variable_Collector(test)
+def report_Generator(region_Codes): 
+    exchange_Rate_Codes = [] 
+    output_Codes = [] 
+    inflation_Codes = [] 
+    for economy, currency, exchange_Rate_Code, exchange_Rate_Source, output_Code, output_Source, inflation_Code, inflation_Source in region_Codes: 
+        exchange_Rate_Codes.append((exchange_Rate_Code, exchange_Rate_Source)) 
+        output_Codes.append((output_Code, output_Source)) 
+        inflation_Codes.append((inflation_Code, inflation_Source)) 
+    exchange_Rates = macro_Variable_Collector(exchange_Rate_Codes) 
+    outputs = macro_Variable_Collector(output_Codes)
+    inflation_Rates = macro_Variable_Collector(inflation_Codes) 
+    counter = 0 
+    euro_Atlantic_Data = panda.DataFrame(columns=['Economy', 'Currency', 'Exchange Rate to USD', 'GDP', 'Inflation Rate'])
+    for economy, currency, exchange_Rate_Code, exchange_Rate_Source, output_Code, output_Source, inflation_Code, inflation_Source in region_Codes: 
+        new_Row = {'Economy': economy, 'Currency': currency, 'Exchange Rate to USD': exchange_Rates[counter], 'GDP': outputs[counter], 'Inflation Rate': inflation_Rates[counter]} 
+        euro_Atlantic_Data = euro_Atlantic_Data.append(new_Row, ignore_index = True) 
+        counter = counter + 1  
+    return euro_Atlantic_Data
 
-#Fetches the exchange rate data for specified currencies, inverts to USD, and adds the most recent observation to the data frame
-#for name, currency_Code in currency_Codes_FromUSD:
-    #exchange_Rate = reader.DataReader(currency_Code, 'fred') 
-    #exchange_Rate_Recent = exchange_Rate.tail(1)[currency_Code][0] 
-    #exchange_Rate_Recent = 1/exchange_Rate_Recent
-    #exchange_Rate_Date = exchange_Rate.tail(1).index.strftime('%Y-%m-%d')[0] 
-    #new_Row = {'Currency': name, 'Currency Code': currency_Code, 'Date': exchange_Rate_Date, 'Exchange Rate to USD': exchange_Rate_Recent} 
-    #exchange_Rate_Data = exchange_Rate_Data.append(new_Row, ignore_index=True)
-
-
-#Returns the data frame containing the most recent exchange rate data from the FRED 
-#tabulate package allows for multiple formats: plain, pretty, psql, html, etc
-print(tabulate(exchange_Rate_Data, headers = 'keys', tablefmt = 'plain'))
+report = report_Generator(euro_Atlantic_Codes) 
+print(tabulate(report, headers = 'keys', tablefmt = 'plain'))
