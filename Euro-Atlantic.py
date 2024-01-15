6272e0a8f89021182354e62df12fbea0311e2be8
hawkeyephil <hawkeyephil@icloud.com>
2024-01-14 21:31:21 -0500

    dataframe to .csv

--- Euro-Atlantic.py
+++ Euro-Atlantic.py
@@ -4,20 +4,19 @@ import pandas as panda
 import numpy as numpy 
 from IPython.display import display 
 from tabulate import tabulate 
+from datetime import date
 
 euro_Atlantic_Codes = [('Canada', 'Canadian Dollar', 'DEXCAUS', 'fred', 'na', 'NAEXKP01CAQ657S', 'fred', 'CANCPALTT01CTGYM', 'fred'), 
                        ('Switzerland', 'Swiss Franc', 'DEXSZUS', 'fred', 'na', 'CHEGDPRQPSMEI', 'fred', 'CHECPALTT01CTGYM', 'fred'), 
                        ('United Kingdom', 'Pound Sterling', 'DEXUSUK', 'fred', 'inversion', 'NAEXKP01GBQ657S', 'fred', 'GBRCPALTT01CTGYM', 'fred'), 
-                       ('European Union', 'Euro', 'DEXUSEU', 'fred', 'inversion', 'NAEXKP01EZQ657S', 'fred', 'CPHPTT01EZM659N', 'fred')]
+                       ('Euro Area', 'Euro', 'DEXUSEU', 'fred', 'inversion', 'NAEXKP01EZQ657S', 'fred', 'CPHPTT01EZM659N', 'fred')]
 
 def macro_Variable_Collector(macro_Variable_Codes): 
     macro_Variables_Dates = panda.DataFrame(columns=['macro_Variable_Recent', 'macro_Variable_Date', 'special_Behavior'])
     for macro_Variable_Code, macro_Variable_Source, special_Behavior in macro_Variable_Codes: 
         macro_Variable = reader.DataReader(macro_Variable_Code, macro_Variable_Source) 
         macro_Variable_Recent = macro_Variable.tail(1)[macro_Variable_Code][0] 
-        #macro_Variable_Recent = "{:.4g}".format(macro_Variable_Recent)
-        macro_Variable_Date = macro_Variable.tail(1).index.strftime('%Y-%m-%d')[0] 
-        #macro_Variable_Recent = str(macro_Variable_Recent)  
+        macro_Variable_Date = macro_Variable.tail(1).index.strftime('%m-%d-%Y')[0]   
         macro_Variable_Date = str(macro_Variable_Date)
         new_Row = {'macro_Variable_Recent': macro_Variable_Recent, 'macro_Variable_Date': macro_Variable_Date, 'special_Behavior': special_Behavior}
         macro_Variables_Dates = macro_Variables_Dates.append(new_Row, ignore_index = True) 
@@ -26,7 +25,6 @@ def macro_Variable_Collector(macro_Variable_Codes):
 def exchange_Rate_Generator(exchange_Rate_Codes):  
     exchange_Rates = macro_Variable_Collector(exchange_Rate_Codes) 
     exchange_Rates_Adjusted = [] 
-    #print(exchange_Rates)
     for index, row in exchange_Rates.iterrows(): 
         if(row["special_Behavior"] == 'inversion'): 
             row["macro_Variable_Recent"] = 1/row["macro_Variable_Recent"]
@@ -37,14 +35,14 @@ def output_Generator(output_Codes):
     outputs = macro_Variable_Collector(output_Codes) 
     outputs_Adjusted = []
     for index, row in outputs.iterrows():        
-        outputs_Adjusted.append(str( "{:.3g}".format(row["macro_Variable_Recent"])) + ' (' + row["macro_Variable_Date"] + ')')
+        outputs_Adjusted.append(str( "{:.3g}".format(row["macro_Variable_Recent"])) + '% (' + row["macro_Variable_Date"] + ')')
     return (outputs_Adjusted) 
 
 def inflation_Rate_Generator(inflation_Rate_Codes): 
     inflation_Rates = macro_Variable_Collector(inflation_Rate_Codes)  
     inflation_Rates_Adjusted = []
     for index, row in inflation_Rates.iterrows(): 
-        inflation_Rates_Adjusted.append(str( "{:.4g}".format(row["macro_Variable_Recent"])) + ' (' + row["macro_Variable_Date"] + ')')
+        inflation_Rates_Adjusted.append(str( "{:.4g}".format(row["macro_Variable_Recent"])) + '% (' + row["macro_Variable_Date"] + ')')
     return (inflation_Rates_Adjusted) 
 
 def report_Generator(region_Codes): 
@@ -59,12 +57,15 @@ def report_Generator(region_Codes):
     outputs = output_Generator(output_Codes)
     inflation_Rates = inflation_Rate_Generator(inflation_Codes) 
     counter = 0 
-    euro_Atlantic_Data = panda.DataFrame(columns=['Economy', 'Currency', 'Exchange Rate to USD', 'GDP', 'Inflation Rate'])
+    euro_Atlantic_Data = panda.DataFrame(columns=['Economy', 'Currency', 'Exchange Rate to USD', 'Percent Change GDP YoY (Seasonally Adjusted)', 'Percent Change CPI YoY (Not Seasonally Adjusted)'])
     for economy, currency, exchange_Rate_Code, exchange_Rate_Source, exchange_Rate_Inversion, output_Code, output_Source, inflation_Code, inflation_Source in region_Codes: 
-        new_Row = {'Economy': economy, 'Currency': currency, 'Exchange Rate to USD': exchange_Rates[counter], 'GDP': outputs[counter], 'Inflation Rate': inflation_Rates[counter]} 
+        new_Row = {'Economy': economy, 'Currency': currency, 'Exchange Rate to USD': exchange_Rates[counter], 'Percent Change GDP YoY (Seasonally Adjusted)': outputs[counter], 'Percent Change CPI YoY (Not Seasonally Adjusted)': inflation_Rates[counter]} 
         euro_Atlantic_Data = euro_Atlantic_Data.append(new_Row, ignore_index = True) 
         counter = counter + 1  
     return euro_Atlantic_Data
 
 report = report_Generator(euro_Atlantic_Codes) 
+today = date.today().strftime('%m-%d-%Y')
+filename = '~/Downloads/Euro-Atlantic Report (' + today + ").csv"
+report.to_csv(filename, index = False)
 print(tabulate(report, headers = 'keys', tablefmt = 'plain')) 
